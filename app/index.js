532fa7adedc5bc7780b9f396b6451651485ba37d
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-10-28 18:55:26 +0530

    Feat: Added  safeareaview & screenheaderbtn section to app

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, SIZES} from '../constants';
-import { Nearbyjobs, Popularjobs, ScreenHeader, Welcome } from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeader, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
@@ -12,9 +12,28 @@ const Home = () => {
       <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite }} >
         <Stack.Screen 
         options={{
-            headerStyle: { backgroundColor:COLORS.lightWhite }
+            headerStyle: { backgroundColor:COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerLeft: () => {
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+            },
+            headerRight: () => {
+                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+            },
+            headerTitle: " "
         }}
         />
+     <ScrollView showsVerticalScrollIndicator={false} >
+        <View style={{
+            flex: 1,
+            padding: SIZES.medium
+        }} >
+     <Welcome />
+     <Popularjobs />
+     <Nearbyjobs />
+        </View>
+     </ScrollView>
+
       </SafeAreaView>      
     );
 };
